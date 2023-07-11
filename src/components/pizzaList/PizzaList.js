5a58e9c487f78227e0011c62caa7873f71d5394f
Evgen <evgen1y-igorevich@yandex.com>
2023-07-11 13:14:05 +0300

    закончил логику корзины

--- src/components/pizzaList/PizzaList.js
+++ src/components/pizzaList/PizzaList.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import PizzaBlock from "../pizzaBlock/PizzaBlock";
-import {useState, useEffect} from "react";
+import {useEffect} from "react";
 import {Skeleton} from "../skeleton/Skeleton";
 import {useDispatch, useSelector} from "react-redux";
-import axios from "axios";
 import qs from 'qs';
 import {useNavigate} from "react-router-dom";
-import {setParams} from "../../redux/slices/filterBtnsSlices";
+import {fetchPizza} from "../../redux/slices/pizzaSlice";
 
 const PizzaList = () => {
     const navigate = useNavigate();
-    const [pizzadb, setPizzaDb] = useState([]);
-    const [loading, setLoading] = useState(true);
 
-    const {value, sort, flag, searchValue, pageCount} = useSelector(state => state.filter)
+    const {value, sort, flag, searchValue, pageCount} = useSelector(state => state.filter);
+    const {pizzas, status} = useSelector(state => state.pizza);
     const dispatch = useDispatch();
 
 
@@ -27,17 +25,13 @@ const PizzaList = () => {
     // },[])
 
     useEffect(() => {
-        setLoading(true);
         const category = value > 0 ? `category=${value}` : '';
         const search = searchValue ? `search=${searchValue}` : '';  //поиск пицц череззапрос на сервер
-
-        axios.get(
-            `https://649fc7e3ed3c41bdd7a6aea8.mockapi.io/items?page=${pageCount}&limit=4&${category}&${search}&sortBy=${sort.sortProperty}&order=desc`
-        )
-        .then(res =>  {
-            setPizzaDb(res.data);
-            setLoading(false);
-        } )
+        dispatch(fetchPizza({
+            pageCount,
+            category,
+            search,
+            sort}))
     }, [value, sort.sortProperty, searchValue, pageCount]);
 
 
@@ -86,12 +80,12 @@ const PizzaList = () => {
 
 
 
-    const visibleItems = searchItemsForInput(pizzadb, searchValue); //получим нас отфильтрованные данные
+    const visibleItems = searchItemsForInput(pizzas, searchValue); //получим нас отфильтрованные данные
     const renderItems = elems(visibleItems) //сюда передаем отфильтрованные данные для рендера наших пицц
 
     const loadSkelet = [...new Array(6)].map((_,i) => <Skeleton key={i}/>); //создание скелетона
 
-    const renderAllItems = loading ? loadSkelet : renderItems;
+    const renderAllItems = status === 'loading' ? loadSkelet : renderItems;
 
     // фильтрация нашего массива. переключение от большего к меньшему и наоборот
     const reversedItems = flag ? [...renderAllItems].reverse() : renderAllItems;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import filtrBtnSlice from './slices/filterBtnsSlices'
 import cartSlice from "./slices/cartSlice";
+import pizzaSlice from "./slices/pizzaSlice";
 
 export const store = configureStore({
     reducer: {
         filter: filtrBtnSlice,
         cart: cartSlice,
+        pizza: pizzaSlice
     },
 })
